a9e10a3e043707a19a297a4a9ff778b3e7cce4fc
Anthony Mangano <amangano@edx.org>
2016-12-15 18:10:12 -0500

    remove already-authenticated-msg selector from bok_choy test page
    Ecom-6107

--- common/test/acceptance/pages/lms/login_and_register.py
+++ common/test/acceptance/pages/lms/login_and_register.py
@@ -356,7 +356,7 @@ class CombinedLoginAndRegisterPage(PageObject):
         """Wait for a status message to be visible following third_party registration, then return it."""
         def _check_func():
             """Return third party auth status notice message."""
-            for selector in ['.already-authenticated-msg p', '.status p']:
+            for selector in ['.status p']:
                 msg_element = self.q(css=selector)
                 if msg_element.visible:
                     return (True, msg_element.text[0])
